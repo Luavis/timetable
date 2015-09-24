8eb039470192087bf394268ac0443436810ef835
Luavis <luaviskang@gmail.com>
2015-09-24 09:21:51 +0900

    Remove i18n

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ from flask.ext.babel import Babel
 app = Flask(__name__)
 app.debug = True
 
-app.config.from_pyfile('locale.cfg')
-babel = Babel(app)
-
-@babel.localselector
-def get_locale():
-    return 'ko'
-
-@babel.timezonselector
-def get_timezon():
-    return None
-
 @app.route('/')
 def index():
     return render_template('index.html')
